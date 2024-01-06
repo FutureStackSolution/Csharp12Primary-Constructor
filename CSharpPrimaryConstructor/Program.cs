e20b887cf442ae76c87d8c03bbc7e3bf6a43a5d8
Jalpesh Vadgama <jalpesh.vadgama@gmail.com>
2024-01-06 11:43:01 +0530

    Intial Commit

---
+++ CSharpPrimaryConstructor/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace CSharpPrimaryConstructor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("C# Primary Contructor");
+        }
+    }
+
+    public class Student
+    {
+        private readonly string _firstName;
+        private readonly string _lastName;
+        private readonly int _standard;
+
+        public Student(string firstName, string lastName, int standard)
+        {
+            _firstName = firstName;
+            _lastName = lastName;
+            _standard = standard;
+        }
+
+        public void PrintStudent()
+        {
+            Console.WriteLine("Printing Student");
+            Console.WriteLine($"FirstName: {_firstName}");
+            Console.WriteLine($"Lastname: {_lastName}");
+            Console.WriteLine($"Standard: {_standard}")
+        }
+    }
+}
