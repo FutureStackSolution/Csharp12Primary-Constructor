717958e103ae321eff9ad4ea2f386d890f5624ce
Jalpesh Vadgama <jalpesh.vadgama@gmail.com>
2024-01-07 18:22:50 +0530

    changes rleated to primary constructor and records

---
+++ CSharpPrimaryConstructor/Student.cs
@@ -0,0 +1,29 @@
+﻿namespace CSharpPrimaryConstructor
+{
+    public class Student(string firstName, string lastName, int standard)
+    {
+        private readonly string _firstName =firstName;
+        private readonly string _lastName=lastName;
+        private readonly int _standard= standard;
+
+        //public Student(string firstName, string lastName, int standard)
+        //{
+        //    _firstName = firstName;
+        //    _lastName = lastName;
+        //    _standard = standard;
+        //}
+
+        public void PrintStudent()
+        {
+            Console.WriteLine("Following is a student details");
+            Console.WriteLine($"FirstName: {_firstName}");
+            Console.WriteLine($"Lastname: {_lastName}");
+            Console.WriteLine($"Standard: {_standard}");
+        }
+    }
+    public record StudentRecord(string firstName, string lastName, int standard)
+    {
+        // business logic and validations there
+    }
+    
+}
